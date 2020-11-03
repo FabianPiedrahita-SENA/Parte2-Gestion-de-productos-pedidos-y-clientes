359100c28f1f973e243b8bdf63fbf3a26193fec5
deivyduvan <duvamelgar2045@gmail.com>
2020-11-03 11:32:52 -0500

    primera parte de views

---
+++ views/productos/insert.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formativo </title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous">
+<link rel="stylesheet" href="css/bootstrap.css">
+
+</head>
+<body>
+
+     <div class="container">
+        <h1>Crear producto</h1>
+
+        <form action="index.php?control=producto&action=store" method="post">
+            <div class="form-group">
+         <label for="nombre">Nombre</label>
+         <input class="form-control" type="text" id="nombre" name="nombre" required>  
+            </div>
+
+         
+
+</body>
+</html>
