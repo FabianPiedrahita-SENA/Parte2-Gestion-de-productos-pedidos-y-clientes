7c807c951d32fe3eea8a579be7fbb59b13a57d3d
alejandror4gtx2020 <cadevia39@misena.edu.co>
2020-11-08 22:03:32 -0500

    Update

---
+++ Login Alejandro Devia/views/home/index.php
@@ -0,0 +1,4 @@
+<?php require_once "views/shared/header.php"; ?> 
+
+<h1>Bienvenido a la sazon del aprendiz </h1>
+
---
+++ Login Alejandro Devia/views/usuario/valide.php
@@ -0,0 +1,26 @@
+
+<h1> Bienvenidos</h1>
+
+<script type="text/javascript">
+    function confirmenvio(){
+
+        var respuesta = confirm("estas seguro de mandar esos datos ?"){
+
+             if(respuesta == true )
+             
+             {
+                
+                return true;
+
+             }
+
+             else{
+
+                 return false;
+             }
+
+        }
+
+        </script>
+
+<button onclick="return confirmenvio()"  type="submit" class="butt">Acceder</button>
